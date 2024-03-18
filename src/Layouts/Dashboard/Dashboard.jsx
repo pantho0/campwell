2a165023f9f3ea9890d838bbed2304147d12addd
pantho0 <panthomashreky6@gmail.com>
2024-03-18 13:01:02 +0600

    added all necessary action button for dashboard

--- src/Layouts/Dashboard/Dashboard.jsx
+++ src/Layouts/Dashboard/Dashboard.jsx
@@ -8,7 +8,9 @@ import {
   ListItemPrefix,
   ListItemSuffix,
   Chip,
+  Tooltip,
 } from "@material-tailwind/react";
+import { HiHome, HiOutlineLogout } from "react-icons/hi";
 import { useState } from "react";
 import Container from "../../Components/Utils/Container";
 import { Outlet } from "react-router-dom";
@@ -158,6 +160,11 @@ const Dashboard = () => {
             <div>
               <h6 className="text-3xl font-semibold text-white text-center mt-5 mb-5">CAMPWELL</h6>
               <hr />
+              <div className="flex justify-center gap-3 mb-5 mt-5">
+                <Tooltip content="Home"><Button className="bg-green-400 hover:bg-green-500"><HiHome size={30} color="white" className="cursor-pointer"/></Button></Tooltip>
+                <Tooltip content="Logout"><Button className="bg-green-400 hover:bg-green-500"><HiOutlineLogout size={30} color="white" className="cursor-pointer"/></Button></Tooltip>
+              </div>
+              <hr className="mb-5"/>
             </div>
             <OrganizerMenu/>
         </div>
